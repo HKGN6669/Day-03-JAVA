b283dd98397cc42f5060d81a76495f2836128f15
HKGN6669 <tayyibafathima666@gmail.com>
2024-12-04 15:22:18 +0530

    Add files via upload

---
+++ smallest3num.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class smallest3num{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        if(a<b&&a<c)
+        System.out.println("a is smallest number");
+        else if(b<a&&b<c)
+        System.out.println("b iss smallest number");
+        else
+        System.out.println("c is smallest number");
+    }
+}
